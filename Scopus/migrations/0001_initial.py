fd93538285274b03c3f0ac72671f725b93d2e557
Joel Nothman <joel.nothman@gmail.com>
2017-08-15 15:00:57 +1000

    Fresh migrations

--- Scopus/migrations/0001_initial.py
+++ Scopus/migrations/0001_initial.py
@@ -13,8 +13,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Abstract',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('abstract', models.TextField(default=b'', help_text=b'The article abstract', max_length=10000)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('abstract', models.TextField(max_length=10000, default='', help_text='The article abstract')),
             ],
             options={
                 'db_table': 'abstract',
@@ -23,16 +23,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Authorship',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('author_id', models.BigIntegerField(help_text=b"Scopus's auid", null=True, db_index=True, blank=True)),
-                ('initials', models.CharField(default=b'', max_length=20)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('author_id', models.BigIntegerField(blank=True, null=True, db_index=True, help_text="Scopus's auid")),
+                ('initials', models.CharField(max_length=20, default='')),
                 ('surname', models.CharField(max_length=100)),
-                ('order', models.PositiveIntegerField(default=0, help_text=b'1 for first author, etc. Can have multiple Authorship entries for one value of order.')),
-                ('affiliation_id', models.IntegerField(help_text=b"Scopus's afid", null=True, db_index=True)),
-                ('organization', models.CharField(default=b'', help_text=b'Name from 1st organization node in affiliation details', max_length=300, db_index=True)),
-                ('department', models.CharField(default=b'', help_text=b'Name from 2nd organization node in affiliation details', max_length=200)),
+                ('order', models.PositiveIntegerField(default=0, help_text='1 for first author, etc. Can have multiple Authorship entries for one value of order.')),
+                ('affiliation_id', models.IntegerField(null=True, db_index=True, help_text="Scopus's afid")),
+                ('organization1', models.CharField(max_length=300, db_index=True, default='', help_text='Name from 1st organization node in affiliation details')),
+                ('organization2', models.CharField(max_length=300, db_index=True, default='', help_text='Name from 2nd organization node in affiliation details')),
+                ('organization3', models.CharField(max_length=300, db_index=True, default='', help_text='Name from 3rd organization node in affiliation details')),
                 ('country', models.CharField(max_length=10)),
-                ('city', models.CharField(help_text=b'Not currently stored', max_length=30)),
+                ('city', models.CharField(max_length=100)),
             ],
             options={
                 'db_table': 'authorship',
@@ -41,9 +42,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Citation',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('cite_to', models.BigIntegerField(default=-1, help_text=b'EID of document being cited', db_index=True)),
-                ('cite_from', models.BigIntegerField(default=-1, help_text=b'EID (or group ID?) of citing document', db_index=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('cite_to', models.BigIntegerField(db_index=True, default=-1, help_text='EID of document being cited')),
+                ('cite_from', models.BigIntegerField(db_index=True, default=-1, help_text='EID (or group ID?) of citing document')),
             ],
             options={
                 'db_table': 'citation',
@@ -52,14 +53,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Document',
             fields=[
-                ('eid', models.BigIntegerField(help_text=b'A unique identifier for the record; but see group_id', serialize=False, primary_key=True, db_index=True)),
-                ('doi', models.CharField(help_text=b'DOI', max_length=150, null=True)),
-                ('pub_year', models.IntegerField(default=-1, help_text=b'Publication year recorded in xocs:pub-year, backing off to xocs:sort-year where pub-year is unavailable', db_index=True)),
-                ('group_id', models.BigIntegerField(help_text=b'An EID shared by likely duplicate doc entries', null=True, db_index=True, blank=True)),
-                ('title', models.CharField(help_text=b'The original (untranslated) title', max_length=500)),
-                ('citation_count', models.IntegerField(default=0, help_text=b'Citation count from citedby.xml')),
-                ('title_language', models.CharField(default=b'', help_text=b'The language of the original title', max_length=5)),
-                ('citation_type', models.CharField(default=b'', help_text=b'The type of document', max_length=5, choices=[(b'ab', b'ab = Abstract Report'), (b'ar', b'ar = Article'), (b'ba', b'ba'), (b'bk', b'bk = Book'), (b'br', b'br = Book Review'), (b'bz', b'bz = Business Article'), (b'cb', b'cb = Conference Abstract'), (b'ch', b'ch = Chapter'), (b'cp', b'cp = Conference Paper'), (b'cr', b'cr = Conference Review'), (b'di', b'di = Dissertation'), (b'ed', b'ed = Editorial'), (b'er', b'er = Erratum'), (b'ip', b'ip = Article in Press'), (b'le', b'le = Letter'), (b'no', b'no = Note'), (b'pa', b'pa = Patent'), (b'pr', b'pr = Press Release'), (b're', b're = Review'), (b'rf', b'rf'), (b'rp', b'rp = Report'), (b'sh', b'sh = Short Survey'), (b'wp', b'wp = Working Paper')])),
+                ('eid', models.BigIntegerField(primary_key=True, db_index=True, serialize=False, help_text='A unique identifier for the record; but see group_id')),
+                ('doi', models.CharField(max_length=150, null=True, help_text='DOI')),
+                ('pub_year', models.IntegerField(db_index=True, default=-1, help_text='Publication year recorded in xocs:pub-year, backing off to xocs:sort-year where pub-year is unavailable')),
+                ('group_id', models.BigIntegerField(blank=True, null=True, db_index=True, help_text='An EID shared by likely duplicate doc entries')),
+                ('title', models.CharField(max_length=500, help_text='The original (untranslated) title')),
+                ('citation_count', models.IntegerField(default=0, help_text='Citation count from citedby.xml')),
+                ('title_language', models.CharField(max_length=5, default='', help_text='The language of the original title')),
+                ('citation_type', models.CharField(max_length=5, default='', choices=[('ab', 'ab = Abstract Report'), ('ar', 'ar = Article'), ('ba', 'ba'), ('bk', 'bk = Book'), ('br', 'br = Book Review'), ('bz', 'bz = Business Article'), ('cb', 'cb = Conference Abstract'), ('ch', 'ch = Chapter'), ('cp', 'cp = Conference Paper'), ('cr', 'cr = Conference Review'), ('di', 'di = Dissertation'), ('ed', 'ed = Editorial'), ('er', 'er = Erratum'), ('ip', 'ip = Article in Press'), ('le', 'le = Letter'), ('no', 'no = Note'), ('pa', 'pa = Patent'), ('pr', 'pr = Press Release'), ('re', 're = Review'), ('rf', 'rf'), ('rp', 'rp = Report'), ('sh', 'sh = Short Survey'), ('wp', 'wp = Working Paper')], help_text='The type of document')),
             ],
             options={
                 'db_table': 'document',
@@ -68,9 +69,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ItemID',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('item_id', models.CharField(help_text=b'The identifier', max_length=20, db_index=True)),
-                ('item_type', models.CharField(help_text=b'ItemID type (see Scopus documentation)', max_length=40)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('item_id', models.CharField(max_length=20, db_index=True, help_text='The identifier')),
+                ('item_type', models.CharField(max_length=40, help_text='ItemID type (see Scopus documentation)')),
                 ('document', models.ForeignKey(to='Scopus.Document')),
             ],
             options={
@@ -80,9 +81,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Source',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('scopus_source_id', models.BigIntegerField(default=-1, help_text=b"Scopus's srcid", db_index=True)),
-                ('source_type', models.CharField(help_text=b'Source type', max_length=1, null=True, db_index=True, choices=[(b'b', b'b = Book'), (b'd', b'd = Trade Journal'), (b'j', b'j = Journal'), (b'k', b'k = Book Series'), (b'm', b'm = Multi-volume Reference Works'), (b'p', b'p = Conference Proceeding'), (b'r', b'r = Report'), (b'n', b'n = Newsletter'), (b'w', b'w = Newspaper')])),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('scopus_source_id', models.BigIntegerField(db_index=True, default=-1, help_text="Scopus's srcid")),
+                ('source_type', models.CharField(max_length=1, null=True, db_index=True, choices=[('b', 'b = Book'), ('d', 'd = Trade Journal'), ('j', 'j = Journal'), ('k', 'k = Book Series'), ('m', 'm = Multi-volume Reference Works'), ('p', 'p = Conference Proceeding'), ('r', 'r = Report'), ('n', 'n = Newsletter'), ('w', 'w = Newspaper')], help_text='Source type')),
                 ('source_title', models.CharField(max_length=400)),
                 ('source_abbrev', models.CharField(max_length=200)),
                 ('issn_print', models.CharField(max_length=15, null=True, db_index=True)),
@@ -99,7 +100,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='document',
             name='source',
-            field=models.ForeignKey(blank=True, to='Scopus.Source', help_text=b'Where the document is published', null=True),
+            field=models.ForeignKey(blank=True, null=True, help_text='Where the document is published', to='Scopus.Source'),
         ),
         migrations.AddField(
             model_name='authorship',
